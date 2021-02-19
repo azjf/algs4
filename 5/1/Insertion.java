1dc9390e3e1d1d4fefe167bb763c27cf268682c3
azjf <a@z.jf>
2021-02-19 21:45:17 +0800

    5.1 String sorts

---
+++ 5/1/Insertion.java
@@ -0,0 +1,19 @@
+public class Insertion {
+	public static void sort(String[] a, int lo, int hi, int d) {
+		for (int i = lo; i <= hi; i++) {
+			for (int j = i; j > lo && less(a[j], a[j - 1], d); j--) {
+				exch(a, j, j - 1);
+			}
+		}
+	}
+
+	private static boolean less(String v, String w, int d) {
+		return v.substring(d).compareTo(w.substring(d)) < 0;
+	}
+
+	private static void exch(Comparable[] a, int i, int j) {
+		Comparable t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+}
